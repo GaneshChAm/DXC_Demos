4f8d7782c854f35fd65db06a9a82fe30a411d757
Gopu <ganeshsivasai.gopu@dxc.com>
2023-04-20 12:40:05 +0530

    updated

--- CSharp/Decision_Making/Program.cs
+++ CSharp/Decision_Making/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Decision making constructs in c#
+//If... Else
+//Switch...Case
+//Arithmetic operators- +,-,*,/,%
+//Comparision operators- ==,<,>,<=,>=,!=
+
+int num;
+Console.WriteLine("Enter a number");
+num=Convert.ToInt16(Console.ReadLine());
+if(num%2 ==0)
+{
+    Console.WriteLine("Number is Even");
+}
+else
+{
+    Console.WriteLine("Number is Odd");
+}
+
+
+int num1,num2;
+Console.WriteLine("Enter First number");
+num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Second number");
+num2 = Convert.ToInt16(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("First number is greater");
+}
+else if (num2 > num1)
+{
+    Console.WriteLine("Second number is greater");
+}
+else
+{
+    Console.WriteLine("Both are equal");
+}
+
+
+//Logical operators- &&,||,!
+//Age>=18 and Qualification>=10
+Console.WriteLine("Enter Your Age");
+int age = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Your Qualification");
+int qual = Convert.ToInt16(Console.ReadLine());
+if (age>=18 && qual>=10)
+{
+    Console.WriteLine("Eligible");
+}
+else
+{
+    Console.WriteLine("Not Eligible");
+}
---
+++ CSharp/Decisionmaking_Activity/Program.cs
@@ -0,0 +1,50 @@
+﻿//To accept 3 numbers from user and display largest among them
+using System.Reflection.Metadata;
+
+int num1, num2, num3;
+Console.WriteLine("Enter First Number");
+num1=Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+num2 =Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Enter Third Number");
+num3 =Convert.ToInt16(Console.ReadLine());
+if (num1 >num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine("First Number is Largest");
+    }
+    else
+    {
+        Console.WriteLine("Third Number is largest");
+    }
+}
+else if (num2 > num3)
+{
+    Console.WriteLine("Second Number is Largest");
+}
+else
+{
+   Console.WriteLine("Third Number is Largest");
+}
+
+
+// To accept a Alphabet check Wheather it is Vowel or Constant
+char A;
+Console.WriteLine("Enter the Alphabet");
+A=Convert.ToChar(Console.ReadLine());
+
+if(A=='a' || A=='e' || A=='i' || A=='o' || A=='u')
+{
+    Console.WriteLine("Given Alphabet is Vowel="+ A);
+}
+else
+{
+    Console.WriteLine("Given Alphabet is Constant="+ A);
+}
+
+
+
+
+
+
